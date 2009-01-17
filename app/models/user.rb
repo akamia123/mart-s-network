6b0b849660e83afd25c4c3d26346aefff6b8135d
Levi Sutton <levisutton@Levi-Suttons-Computer-2.local>
2009-01-16 23:54:15 -0500

    More Basics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :user_groups
+  has_many :groups, :through => :user_groups
+  has_many :group_posts
+  has_many :galleries
+  has_many :pictures
+  has_many :posts
+  has_many :messages
+  has_many :comments
 end
